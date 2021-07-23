874dd7938bc3573d3b45e4fa13f2660133fab375
Xiaoqz <360747356@qq.com>
2021-07-23 20:18:50 +0800

    first commit

---
+++ src/First.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author xiaoqz
+ * @create 2021-07-23 20:16
+ * @description
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("first git commit");
+    }
+}
