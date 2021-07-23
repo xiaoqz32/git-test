489504aa8948c9029ac119093cfeb632b1d0c8d9
Xiaoqz <360747356@qq.com>
2021-07-23 20:27:54 +0800

    master commit

--- src/First.java
+++ src/First.java
@@ -9,5 +9,6 @@ public class First {
     public static void main(String[] args) {
         System.out.println("first git commit");
         System.out.println("second git commit");
+        System.out.println("master commit");
     }
 }
